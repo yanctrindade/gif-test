54caf9dcb7e962731880fb8fe7fd458af09f89fd
YAN CORREA TRINDADE <trindade.yan@gmail.com>
2022-02-01 15:12:13 -0300

    [Main] get first trending gifs

---
+++ Dispo Take Home/Screens/Main/GifTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  GifTableViewCell.swift
+//  Dispo Take Home
+//
+//  Created by Yan Correa Trindade on 01/02/22.
+//
+
+import UIKit
+
+class GifTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
